28b4998ad4c1e21c3b0af0fdd18dc8250b027c1a
Senbaga Kumar Sigamani (Infosys Ltd) <v-sesiga@microsoft.com>
2018-08-27 12:34:00 -0700

    Welding Project Added in the Repository

---
+++ Projects/NetProject/DesktopApplication/WeldingDataCollector/WeldingDataCollector/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WeldingDataCollector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtVoltage = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCurrent = new System.Windows.Forms.TextBox();
+            this.txtIdleTime = new System.Windows.Forms.TextBox();
+            this.txtWeldingTime = new System.Windows.Forms.TextBox();
+            this.txtWeldingCount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(187, 361);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 47);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(336, 361);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(92, 46);
+            this.btnDisconnect.TabIndex = 1;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // txtVoltage
+            // 
+            this.txtVoltage.Location = new System.Drawing.Point(229, 97);
+            this.txtVoltage.Name = "txtVoltage";
+            this.txtVoltage.Size = new System.Drawing.Size(245, 22);
+            this.txtVoltage.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(125, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Voltage";
+            // 
+            // txtCurrent
+            // 
+            this.txtCurrent.Location = new System.Drawing.Point(229, 142);
+            this.txtCurrent.Name = "txtCurrent";
+            this.txtCurrent.Size = new System.Drawing.Size(245, 22);
+            this.txtCurrent.TabIndex = 4;
+            // 
+            // txtIdleTime
+            // 
+            this.txtIdleTime.Location = new System.Drawing.Point(229, 190);
+            this.txtIdleTime.Name = "txtIdleTime";
+            this.txtIdleTime.Size = new System.Drawing.Size(245, 22);
+            this.txtIdleTime.TabIndex = 5;
+            // 
+            // txtWeldingTime
+            // 
+            this.txtWeldingTime.Location = new System.Drawing.Point(229, 240);
+            this.txtWeldingTime.Name = "txtWeldingTime";
+            this.txtWeldingTime.Size = new System.Drawing.Size(245, 22);
+            this.txtWeldingTime.TabIndex = 6;
+            // 
+            // txtWeldingCount
+            // 
+            this.txtWeldingCount.Location = new System.Drawing.Point(229, 290);
+            this.txtWeldingCount.Name = "txtWeldingCount";
+            this.txtWeldingCount.Size = new System.Drawing.Size(245, 22);
+            this.txtWeldingCount.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(125, 295);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "WeldingCount";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(125, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "WeldingTime";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(125, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "IdleTime";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(125, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Current";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 623);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtWeldingCount);
+            this.Controls.Add(this.txtWeldingTime);
+            this.Controls.Add(this.txtIdleTime);
+            this.Controls.Add(this.txtCurrent);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVoltage);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtVoltage;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCurrent;
+        private System.Windows.Forms.TextBox txtIdleTime;
+        private System.Windows.Forms.TextBox txtWeldingTime;
+        private System.Windows.Forms.TextBox txtWeldingCount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Projects/NetProject/DesktopApplication/WeldingDataCollector/WeldingDataCollector/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeldingDataCollector
+{
+    public partial class Form1 : Form
+    {
+        private CancellationToken _cancellationToken;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        string _Server = string.Empty;
+        int _Port = 0;
+
+        Func<string, string> reader = key => ConfigurationManager.AppSettings[key];
+        string strFileName = "";
+        string strFolderPath = "";
+        string strFilePath = "";
+        string baseDirectory = "";
+        int interval = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            _cancellationTokenSource = new CancellationTokenSource();
+            _cancellationToken = _cancellationTokenSource.Token;
+
+            _Server = reader("Server");
+            _Port = int.Parse(reader("Port"));
+            baseDirectory = reader("DirectoryPath");
+            interval = int.Parse(reader("Interval"));
+
+            strFileName = DateTime.Now.ToString("HH-mm-ss");
+            strFolderPath = DateTime.Now.ToString("dd-MMM-yyyy");
+
+            timer1.Interval = interval;           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                timer1.Enabled = true;             
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;            
+        }
+
+        private void OnExecutor()
+        {
+            //AssignStreamValue();
+            using (TcpClient client = new TcpClient(_Server, _Port))
+            {
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] data = new Byte[256];
+                    string requestData = string.Empty;
+
+                    try
+                    {
+                        int bytes = stream.Read(data, 0, data.Length);
+                        requestData = Encoding.ASCII.GetString(data, 0, bytes);
+
+                        if (!string.IsNullOrEmpty(requestData))
+                            AssignStreamValue(requestData);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        throw ex;
+                    }
+                }
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.OnExecutor();
+        }
+
+        private void AssignStreamValue(string streamValue="")
+        {
+            string loc = streamValue ;//"[58-570-40-60-55]";
+            loc=loc.Replace("[", "");
+            loc=loc.Replace("]", "");
+            string[] stream = loc.Split('-');
+            if (stream.Length > 0)
+            {
+                txtVoltage.Text = stream[0];
+                txtCurrent.Text = stream[1];
+                txtIdleTime.Text = stream[2];
+                txtWeldingTime.Text = stream[3];
+                txtWeldingCount.Text = stream[4];
+
+                WriteRecordInCSV(stream);
+            }
+            Thread.Sleep(1000);
+        }
+
+        private void WriteRecordInCSV(string[] stream)
+        {
+            strFilePath = baseDirectory + @"\" + strFolderPath + @"\" + strFileName + ".csv";
+            bool firstCheck = false;
+            if (!Directory.Exists(baseDirectory + @"\" + strFolderPath))
+            {
+                Directory.CreateDirectory(baseDirectory + @"\" + strFolderPath);
+                firstCheck = true;
+            }
+            using (FileStream fs = new FileStream(strFilePath, FileMode.Append, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                if (firstCheck)
+                    sw.WriteLine("Voltage,Current,IdleTime,WeldingTime,WeldingCount,");
+
+                if (!firstCheck)
+                    sw.Write(sw.NewLine);
+
+                foreach (string s in stream)
+                {
+                    sw.Write(s + ",");
+                }
+                sw.Write(sw.NewLine);
+                sw.Flush();
+                sw.Close();
+            }
+            
+        }        
+            
+    }
+}
