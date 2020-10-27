d2d1f2717660d40e00af9e308150323026cfe9df
Senbaga Kumar <senbagakumar@live.com>
2020-10-26 20:07:10 -0700

    Added files

---
+++ Demos/InterviewPrepration/Prepration/Prepration/OnLineAssessment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prepration
+{
+    class OnLineAssessment
+    {
+        //https://leetcode.com/discuss/interview-question/398023/Microsoft-Online-Assessment-Questions/
+        //https://leetcode.com/discuss/interview-question/344650/Amazon-Online-Assessment-Questions
+
+    }
+}
