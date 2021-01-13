f4ff35ad90e43a7769016423601ad1c174ad2279
Senbaga Kumar Sigamani (Infosys Ltd) <v-sesiga@microsoft.com>
2021-01-13 15:37:41 -0800

    Added another program.

---
+++ Demos/InterviewPrepration/Prepration/Prepration/Amazon/OnlineAssessment/UtilizationCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prepration.Amazon.OnlineAssessment
+{
+    class UtilizationCheck
+    {
+        private static int Limit = 2 * 100_100_1000;
+        private static int UtilizationChecks(int instances, List<int> utilizationUtil)
+        {
+            for (int i = 0; i < utilizationUtil.Count; i++)
+            {
+                int util = utilizationUtil[i];
+                if (util < 25 && instances > 1)
+                {
+                    instances = instances / 2 + (instances & 1);
+                    i += 10;
+                }
+                else if (util > 60)
+                {
+                    int newInstances = instances * 2;
+                    if (newInstances <= Limit)
+                    {
+                        instances = newInstances;
+                        i += 10;
+                    }
+                }
+            }
+            return instances;
+        }
+    }
+}
--- Demos/InterviewPrepration/Prepration/Prepration/Program.cs
+++ Demos/InterviewPrepration/Prepration/Prepration/Program.cs
@@ -189,10 +189,52 @@ namespace Prepration
             return -1;
         }
 
-        
+        public int MinDifficulty(int[] jobDifficulty, int d)
+        {
+            checked
+            {
+                long[,] dp = new long[jobDifficulty.Length + 1, d + 1];
+
+                for (int i = 1; i <= jobDifficulty.Length; i++)
+                {
+                    dp[i, 0] = int.MaxValue;
+                }
+
+                for (int i = 1; i <= d; i++)
+                {
+                    dp[0, i] = int.MaxValue;
+                }
+
+                for (int i = 1; i <= jobDifficulty.Length; i++)
+                {
+                    for (int j = 1; j <= d; j++)
+                    {
+                        dp[i, j] = int.MaxValue;
+
+                        if (j > i)
+                        {
+                            continue;
+                        }
+
+                        int max = int.MinValue;
+                        for (int k = i; k >= 1; k--)
+                        {
+                            max = Math.Max(max, jobDifficulty[k - 1]);
+                            dp[i, j] = Math.Min(dp[i, j], dp[k - 1, j - 1] + max);
+                        }
+
+                    }
+                }
+
+                var res = dp[jobDifficulty.Length, d];
+                return res >= int.MaxValue ? -1 : (int)res;
+            }
+        }
 
         static void Main(string[] args)
         {
+            new Program().MinDifficulty(new int[] { 11, 111, 22, 222, 33, 333, 44, 444 }, 6);
+
             var newEdges = new int[3][];
             newEdges[0] = new int[] { 1, 2, 12 };
             newEdges[1] = new int[] { 3, 4, 30 };
